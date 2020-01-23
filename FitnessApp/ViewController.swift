e6f719b0a86ebf6357d768571e663592ebf25313
Станислав Шияновский <st-small@rambler.ru>
2020-01-23 19:47:25 +0200

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Stanly Shiyanovskiy on 23.01.2020.
+//  Copyright © 2020 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
